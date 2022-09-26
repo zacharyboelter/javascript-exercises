7860c0a882ed9ec9a67c58c416801fb1ba20c2a1
Zachary Boelter <zboelter@gmail.com>
2022-09-26 15:25:55 -0700

    repeatString works

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num > 0){
+        return str.repeat(num)
+    }
+    else if (num < 0){
+        return 'ERROR'
+    } else{
+        return ''
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
