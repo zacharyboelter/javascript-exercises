b9ae9879e5fa17d316724e90431d0dff0468dcb7
Zachary Boelter <zboelter@gmail.com>
2022-09-27 14:54:30 -0700

    sumAll func ~ not as easy as seemed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    //if not a number
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR'
+    //if negative number
+    if (x < 0 || y < 0) return 'ERROR'
+    //if x is greater than y
+    if (x > y){
+        let min = x
+         x = y
+         y = min
+    }
 
+
+    //add smaller number to sum until 1 more than larger num
+    let sum = 0
+    for (let i = x; i < y + 1; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
