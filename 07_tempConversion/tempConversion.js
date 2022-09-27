6ffe0679b3ce36713f07fbac18817fb5fbd6450b
Zachary Boelter <zboelter@gmail.com>
2022-09-27 15:52:36 -0700

    tempConversion function working both ways

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  // toFixed returns string, parseFloat for decimal point
+  let answerF = ((f - 32) * 5/9).toFixed(1)
+  return parseFloat(answerF)
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let answerC = (c * 9/5 + 32).toFixed(1)
+  return parseFloat(answerC)
 };
 
 // Do not edit below this line
@@ -11,3 +14,8 @@ module.exports = {
   ftoc,
   ctof
 };
+
+
+
+// multiply by 1.8 (or 9/5) and add 32.
+// subtract 32 and multiply by .5556
