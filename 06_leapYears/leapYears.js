00b33a2266bcb670b1478b82da26f1e8793ec8bb
Zachary Boelter <zboelter@gmail.com>
2022-09-27 15:40:26 -0700

    leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+        //if year / 4 is 0 and year / 100 isnt 0 or year / 400 IS 0, return true
+    if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
